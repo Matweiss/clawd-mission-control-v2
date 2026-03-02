3b8799930fd7aa1305d7e9432ada7954c4eae1bb
Kimi Claw <kimi-claw@matweiss.com>
2026-03-03 05:02:01 +0800

    Fix PetTrackerPanel prop error - removed onRefresh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -312,7 +312,7 @@ export default function MissionControl() {
 
             <ActivityPanel activities={[]} />
             
-            <PetTrackerPanel onRefresh={refresh} />
+            <PetTrackerPanel />
 
             <CalendarPanel events={calendarEvents || []} />
 
